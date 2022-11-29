6da3f708a082295a48d6607fc5252b25f5438d06
AlexTr11 <alex.travez1364@utc.edu.ec>
2022-11-29 15:49:30 -0500

    Vistas, Encabezado y Pie

---
+++ application/views/pie.php
@@ -0,0 +1,19 @@
+<br><br><br><br>
+<div class="row" style="background-color: #1B1F4C">
+  <div class="col-md-6"> <br> <br><br>
+    <p class="text-center " style="color:#FFFFFF">UNIVERSIDAD TÉCNICA DE COTOPAXI</p>
+    <p class="text-center" style="color:#FFFFFF">DESARROLLADO POR ALEX TRAVEZ U EDITH CUMBAJIN</p>
+    <i class=""></i>
+
+  </div>
+  <div class="col-md-6" style="background-color: #FF3333"  > <br><br><br>
+    <p class="text-center " style="color:#FFFFFF">Dirección: 39HQ+4V3, Latacunga</p>
+    <p class="text-center" style="color:#FFFFFF">Teléfono: (03) 280-1147</p>
+    <i class=""></i>
+
+  </div>
+
+</div>
+
+</body>
+</html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-	<head>
-		<meta charset="utf-8">
-		<title>PRIMER CAMBIO</title>
-	</head>
-	<body>
-		<h1>Bienvenidos</h1>
-	</body>
-</html>
+
